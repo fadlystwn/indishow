fb244cdbda2a7a9b760a7464f0446c9e8782691a
fadlydev <fadlystwn@gmail.com>
2025-04-02 07:28:59 +0700

    update user sign up and login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |u|
+      u.name = auth.info.name
+      u.email = auth.info.email
+      u.avatar_url = auth.info.image
+    end
+    session[:user_id] = user.id
+    redirect_to root_path, notice: 'Signed in!'
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Signed out!'
+  end
+end
