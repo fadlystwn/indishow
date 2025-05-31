8ff4f1f70b57ce2a7970e5c51f46fdb347de0462
fadlydev <fadlystwn@gmail.com>
2025-05-31 23:59:01 +0700

    Implement artist profile management with show, edit, and update actions; add views for editing and displaying artist profiles, including form handling and authorization checks.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
 
+  resources :artist_profiles, path: 'artists', only: [:show, :edit, :update]
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
