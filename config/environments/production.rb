c81d7214c42ee0e66977669a5ee795bad943c894
fadlydev <fadlystwn@gmail.com>
2025-07-26 14:21:00 +0700

    Fix production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,13 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-  config.hosts << "skenaria-production.up.railway.app"
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "skenaria-production.up.railway.app",
+    "skenaria.up.railway.app"
+  ]
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
